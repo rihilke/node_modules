35dc020ec23c4429e54572ba57e1118bbcbfb0d1
Aleks Rihilke <a.i.kochergina@yandex.ru>
2022-07-13 01:16:35 +0300

    file server

--- file_server.js
+++ file_server.js
@@ -26,7 +26,7 @@ async function notAllowed(request) {
 const {parse} = require("url");
 const {resolve, sep} = require("path");
 
-const baseDirectory = process.csw();
+const baseDirectory = process.cwd();
 
 function urlPath(url) {
     let {pathname} = parse(url);
@@ -58,4 +58,53 @@ methods.GET = async function(request) {
         return {body: createReadStream(path),
                 type: mime.getType(path)};
     }
+};
+
+const {rmdir, unlink} = require("fs").promises;
+
+methods.DELETE = async function(request) {
+    let path = urlPath(request.url);
+    let stats;
+    try {
+        stats = await stat(path);
+    } catch (error) {
+        if (error.code != "ENOENT") throw error;
+        else return {status: 204};
+    }
+    if (stats.isDirectory()) await rmdir(path);
+    else await unlink(path);
+    return {status: 204};
+};
+
+const {createWriteStream} = require("fs");
+
+function pipeStream(from, to) {
+    return new Promise((resolve, reject) => {
+        from.on("error", reject);
+        to.on("error", reject);
+        to.on("finish", resolve);
+        from.pipe(to);
+    });
+}
+
+methods.PUT = async function(request) {
+    let path = urlPath(request.url);
+    await pipeStream(request, createWriteStream(path));
+    return {status: 204};
+};
+
+const {mkdir} = require("fs").promises;
+
+methods.MKCOL = async function(request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+    await mkdir(path);
+    return {status: 204};
+  }
+  if (stats.isDirectory()) return {status: 204};
+  else return {status: 400, body: "Not a directory"};
 };
